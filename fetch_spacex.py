7cae1dfd0dc49c1856547cece962c8166491e3c3
art-litv <70752711+art-litv@users.noreply.github.com>
2020-09-14 12:19:37 +0300

    spacex_fixed
    Fixed pep8, --count is required now

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,63 +5,56 @@ import argparse
 
 
 def get_spacex_launch_images_urls(max_counts: int, last_image_index=0) -> list:
-  ''' Returns a list of urls of SpaceX flights images '''
-  spacex_api_url = 'https://api.spacexdata.com/v4/launches'
+    ''' Returns a list of urls of SpaceX flights images '''
+    spacex_api_url = 'https://api.spacexdata.com/v4/launches'
 
-  response = requests.get(spacex_api_url)
-  response.raise_for_status()
+    response = requests.get(spacex_api_url)
+    response.raise_for_status()
 
-  spacex_flights = response.json()
-  spacex_images_urls = []
+    spacex_flights = response.json()
+    spacex_images_urls = []
 
-  count = 0
-  for spacex_flight in spacex_flights:
-    flight_images_urls = spacex_flight['links']['flickr']['original']
-    for spacex_image_url in flight_images_urls:
-      if count == int(max_counts) + last_image_index:
-        break
-      spacex_images_urls.append(spacex_image_url)
-      count += 1
+    count = 0
+    for spacex_flight in spacex_flights:
+        flight_images_urls = spacex_flight['links']['flickr']['original']
+        for spacex_image_url in flight_images_urls:
+            if count == int(max_counts) + last_image_index:
+                break
+            spacex_images_urls.append(spacex_image_url)
+            count += 1
 
-  return spacex_images_urls[last_image_index:]
+    return spacex_images_urls[last_image_index:]
 
 
 def download_spacex_launch_images(max_counts):
-  ''' Downloads images of SpaceX spaceships '''
-  image_counter = 0
+    ''' Downloads images of SpaceX spaceships '''
+    image_counter = 0
 
-  spacex_images_urls = get_spacex_launch_images_urls(max_counts)
-  if os.path.exists('images/spacex'):
-    if len(os.listdir('images/spacex')) == 0:
-      pass
-    else:
-      last_image_index = max([int(image_title.strip('spacex_img').split('.')[0]) for image_title in os.listdir('images/spacex')])
-      image_counter = last_image_index
-      spacex_images_urls = get_spacex_launch_images_urls(max_counts, last_image_index)
+    spacex_images_urls = get_spacex_launch_images_urls(max_counts)
+    if os.path.exists('images/spacex'):
+        if len(os.listdir('images/spacex')) == 0:
+            pass
+        else:
+            last_image_index = max([int(image_title.strip('spacex_img').
+                                    split('.')[0]) for image_title
+                                    in os.listdir('images/spacex')])
+            image_counter = last_image_index
+            spacex_images_urls = get_spacex_launch_images_urls(
+                max_counts, last_image_index)
 
-  status_counter = 0
-  for spacex_image_url in spacex_images_urls:
-    image_counter += 1
-    download_image(spacex_image_url, f'spacex/spacex_img{image_counter}')
-    status_counter += 1
-    if status_counter == 1:
-        print(f"Downloaded {status_counter} SpaceX image")
-    elif status_counter > 1:
-        print(f"Downloaded {status_counter} SpaceX images")
+    for spacex_image_url in spacex_images_urls:
+        image_counter += 1
+        download_image(spacex_image_url, f'spacex/spacex_img{image_counter}')
 
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--count", help="count of spacex images to download")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--count", help="count of spacex images to download",
+    required=True)
+    args = parser.parse_args()
 
-  if args.count is None:
-    exit("You haven't choosen any arguments")
-
-  download_spacex_launch_images(args.count)
-
-  print("\nDone")
+    download_spacex_launch_images(args.count)
 
 
 if __name__ == '__main__':
-  main()
+    main()
