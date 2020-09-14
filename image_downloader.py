c85f5cf83e53b0456866cfe0971b33e4fc5a5bc2
art-litv <70752711+art-litv@users.noreply.github.com>
2020-09-14 12:25:28 +0300

    downloader_fixed
    Fixed pep8, got rid of unnecessary lambda function.

--- image_downloader.py
+++ image_downloader.py
@@ -3,25 +3,23 @@ import os
 import urllib3
 
 
-get_file_extension = lambda url: '.' + url.split(".")[-1]
-
-
 def download_image(url: str, path: str, verify=True):
-  ''' Downloads an image into "images" directory '''
-  urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    ''' Downloads an image into "images" directory '''
+    ''' Required for fetch_spacex.py and fetch_hubble.py '''
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-  response = requests.get(url, verify=verify)
-  response.raise_for_status()
+    response = requests.get(url, verify=verify)
+    response.raise_for_status()
 
-  filename = path.split('/')[-1]
-  directories = path[0:path.find(filename)]
+    filename = path.split('/')[-1]
+    directories = path[0:path.find(filename)]
 
-  try:
-    os.makedirs(f"images/{directories}", exist_ok=False)
-  except FileExistsError:
-    pass
+    try:
+        os.makedirs(f"images/{directories}", exist_ok=False)
+    except FileExistsError:
+        pass
 
-  path = path + get_file_extension(url)
+    path = path + '.' + url.split(".")[-1]
 
-  with open(f"images/{path}", 'wb') as file:
-    file.write(response.content)
+    with open(f"images/{path}", 'wb') as file:
+        file.write(response.content)
