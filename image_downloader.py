a67d25d63fb4df8f8d718c54211625aa1589e692
art-litv <70752711+art-litv@users.noreply.github.com>
2020-09-16 16:43:17 +0300

    Fixed downloader
    Made path separator adaptable for different OS

--- image_downloader.py
+++ image_downloader.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import urllib3
+from pathlib import PurePath
 
 
 def download_image(url: str, path: str, verify=True):
@@ -11,15 +12,15 @@ def download_image(url: str, path: str, verify=True):
     response = requests.get(url, verify=verify)
     response.raise_for_status()
 
-    filename = path.split('/')[-1]
+    filename = path.split(os.sep)[-1]
     directories = path[0:path.find(filename)]
 
     try:
-        os.makedirs(f"images/{directories}", exist_ok=False)
+        os.makedirs(f"images{os.sep}{directories}", exist_ok=False)
     except FileExistsError:
         pass
 
     path = path + '.' + url.split(".")[-1]
 
-    with open(f"images/{path}", 'wb') as file:
+    with open(f"images{os.sep}{path}", 'wb') as file:
         file.write(response.content)
